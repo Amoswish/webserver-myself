ddd38bc86af787611ab47721c0b67434e67d0685
amoswish <1572912653@qq.com>
2020-06-30 00:01:40 +0800

    修改git配置

--- webserver-myself/webclient/DerivedData/webclient/Build/Intermediates.noindex/webclient.build/Debug/webclient.build/Objects-normal/x86_64/main.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/wuhao/lab/echo_server/webserver-myself/webclient/webclient/main.cpp \
-  /Users/wuhao/lab/echo_server/webserver-myself/webclient/webclient/Tcp_Client.h \
-  /Users/wuhao/lab/echo_server/webserver-myself/webclient/webclient/TcpMessage.h \
-  /Users/wuhao/lab/echo_server/webserver-myself/webclient/webclient/CELLTimestamp.h
